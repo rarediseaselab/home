e19adb22a85f58d01702d46c65ee8e1ca8892cb2
rarediseaselab <sebihacevik@gmail.com>
2025-06-26 14:24:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function loadCiliaHubData() {
     try {
-        const response = await fetch('./ciliahub_data.json');
+        const response = await fetch('https://raw.githubusercontent.com/rarediseaselab/home/main/ciliahub_data.json');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
